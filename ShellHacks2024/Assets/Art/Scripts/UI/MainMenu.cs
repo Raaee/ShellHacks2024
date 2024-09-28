0e42ac651fb9941a155c5a42abc0a7951c8a5516
Ally <Roa.ayleen07@gmail.com>
2024-09-28 17:15:42 -0400

    Created Main Menu

---
+++ ShellHacks2024/Assets/Art/Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
+
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
+
